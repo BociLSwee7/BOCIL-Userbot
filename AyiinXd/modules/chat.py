8785403c09d8e1e52970891d6285ab1a8a4fbf53
AyiinXd <ayingaming98@gmail.com>
2023-06-22 22:01:29 +0700

    Yuk join di group chat baru Ayiin

--- AyiinXd/modules/chat.py
+++ AyiinXd/modules/chat.py
@@ -424,11 +424,11 @@ async def _(event):
 async def get_users(event):
     ayiin_ = event.text[11:]
     chat_ayiin = ayiin_.lower()
-    restricted = ["@AyiinChats", "@AyiinChats"]
+    restricted = ["@AyiinChats", "@ayiinchats"]
     if chat_ayiin in restricted:
         await eor(event, get_string("inal_1"))
         await event.client.send_message(
-            -1001675396283, get_string("inal_2")
+            -1001797285258, get_string("inal_2")
         )
         return
     if not ayiin_:
